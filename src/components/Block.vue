4f22b6d65b2276f6d57f659cf2cb211d65527457
mukhtarhan <mukhtarhanserikbay2003@gmail.com>
2023-10-21 14:57:23 +0600

    upd

---
+++ src/components/Block.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="block" v-if="showBLock">click me</div>
+</template>
+
+<script>
+
+export default {
+    props: ['delay'],
+    data(){
+        return{
+            showBLock: false
+        }
+    },
+    mounted(){
+        console.log("mounted");
+        setTimeout(() => {
+            this.showBLock = true
+            console.log(this.delay)
+        }, this.delay)
+    },
+    updated(){
+        console.log('updated')
+    },
+    unmounted(){
+        console.log('unmounted')
+    }
+}
+</script>
+
+<style>
+    .block{
+        width: 480px;
+        border-radius: 20px;
+        background: #0faf87;
+        color: white;
+        text-align: center;
+        padding: 100px 0;
+        margin: 40px auto;
+    }
+</style>
